7ca21f66a720f4459b50fb495a597b98bde6ed8c
chinmayagumaste <chinmaygumaste@gmail.com>
2026-06-07 11:18:09 +0530

    chinmaya test

---
+++ newprogram.py
@@ -0,0 +1,12 @@
+def add (a, b):
+    result = a + b
+    return result
+
+def main ():
+    num1 = 5
+    num2 = 10
+    sum_result = add(num1, num2)
+    print(f"The sum of {num1} and {num2} is: {sum_result}")
+
+if __name__ == "__main__":
+    main() 
