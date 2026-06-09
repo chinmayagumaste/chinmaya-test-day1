50db6f28177e09977d3a2f8121ff4fb2610c3cff
chinmayagumaste <chinmaygumaste@gmail.com>
2026-06-09 13:07:31 +0530

    Day 5 Dic

--- day4dicrion_1.py
+++ day4dicrion_1.py
@@ -5,5 +5,9 @@ list = {
 "Toipcs":(list, "tuple", "dictionary"),
 9:2.69
 }
+list["name"]="Madhur" #Overwrite the value of name key
+list["surname"]="Gumaste"
 print(list["name"])
 print(list[9])
+print(list["surname"])
+print(list)
