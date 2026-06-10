66138eafc73c2d1bda1d1db304b64ed45c29be63
chinmayagumaste <chinmaygumaste@gmail.com>
2026-06-10 12:02:27 +0530

    Day 5 dic and set

---
+++ day5_set.py
@@ -0,0 +1,6 @@
+collection ={"1", "2", "3", "4"}
+collection2 ={"4", "5", "6", "7"}
+
+print(collection.union(collection2)) # union() method returns a new set that contains all the elements from both sets, without duplicates.
+print(collection.intersection(collection2)) # intersection() method returns a new set that contains only the elements that are present in both sets.
+print(collection.difference(collection2)) # difference() method returns
---
+++ day_5_dic.py
@@ -0,0 +1,10 @@
+dic = {
+"cat" : "a small animal",
+"table": ["a price", "a piece of furniture"],
+"apple": "a fruit",
+}
+print(dic)
+print(dic["cat"])
+print(dic["table"])
+print(dic["table"][0])
+print(dic["table"][1])  
