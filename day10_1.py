ab6ff619ea2fd717a31af5a8413d83b3abc247fe
chinmayagumaste <chinmaygumaste@gmail.com>
2026-06-16 22:03:28 +0530

    Day 10 OOPS

---
+++ day10_1.py
@@ -0,0 +1,12 @@
+class Student :
+    def __init__(self,name,marks):
+        self.name=name
+        self.marks=marks
+        print("Adding new student in Database..")
+s1=Student("Karan",97)
+print (s1.name,s1.marks)
+
+s2=Student ("Madhurmanoj",100)
+print (s2.name , s2.marks,)
+
+        
---
+++ day9_3.py
@@ -0,0 +1,3 @@
+with open ("Test.txt","w") as f :
+    f.write("Hi every one\nWe are test this filr creation\n")
+    f.write("New Line test\n4th Line test")
